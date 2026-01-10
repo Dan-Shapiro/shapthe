b8bbb5c893391629d9ff60fefced4d78f246628a
Dan Shapiro <ds.dan.shapiro@gmail.com>
2026-01-10 17:54:50 -0500

    Add faction and player mat setup stored in initial state

---
+++ app/services/engine/catalog.rb
@@ -0,0 +1,23 @@
+module Engine
+  module Catalog
+    FACTIONS = [
+      "Nordic",
+      "Rusviet",
+      "Saxony",
+      "Polania",
+      "Crimea",
+      "Albion",
+      "Togawa"
+  ].freeze
+
+  PLAYER_MATS = [
+    "Industrial",
+    "Engineering",
+    "Patriotic",
+    "Mechanical",
+    "Agricultural",
+    "Militant",
+    "Innovative"
+].freeze
+  end
+end
